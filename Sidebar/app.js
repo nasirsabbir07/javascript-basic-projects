525c4cac78b78371e6accb4146973b29945c0d1d
Nasir Uddin Sabbir <nasirsabbir07@gmail.com>
2021-07-26 02:40:02 +0600

    Added js to toggle the sidebar

---
+++ Sidebar/app.js
@@ -0,0 +1,17 @@
+const sidebarToggle = document.querySelector(".sidebar-toggle");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+
+sidebarToggle.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+  /*
+  if (sidebar.classList.contains("show-sidebar")) {
+    sidebar.classList.remove("show-sidebar");
+  } else {
+    sidebar.classList.add("show-sidebar");
+  }*/
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
